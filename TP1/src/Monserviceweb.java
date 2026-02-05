849453a7a6e1d28cd18a33d00f31f60189df39c2
Sabrina <zeghoudsabrina24@gmail.com>
2026-02-05 14:15:17 +0100

    TP1:Développement et test d’un service web SOAP en Java

---
+++ TP1/src/Monserviceweb.java
@@ -0,0 +1,20 @@
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+@WebService(targetNamespace = "http://www.polytech.fr")
+public class Monserviceweb  {
+    @WebMethod(operationName = "convertir")
+    public double conversion(double mt){
+        return mt*0.9;
+
+    }
+    public double somme(@WebParam(name="parametre1") double a, double b) {
+        return a+b;
+    }
+
+    public Etudiant getEtudiant(int identifiant){
+        return new Etudiant(1, "Mario", 19);
+
+    }
+
+}
